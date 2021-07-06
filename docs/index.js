bb34ca45314f400ac03f852c8a18f0335ff3e7f8
Mohammed Khan <khanmohammed1@fb.com>
2021-07-06 16:31:30 -0700

    Added the feature that will close the modal when you click outside of it

--- docs/index.js
+++ docs/index.js
@@ -8,3 +8,8 @@ document.querySelector('#Book').addEventListener('click', function() {
     modalMask.style.display= 'none'
    })
 
+   document.querySelector('#modal-mask').addEventListener('click', function() {
+    let modalMask = document.querySelector('#modal-mask')
+    modalMask.style.display= 'none'
+   })
+

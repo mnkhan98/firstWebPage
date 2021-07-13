2ec616f190fd301b2181a1d7efe4647878dac334
Mohammed Khan <khanmohammed1@fb.com>
2021-07-13 11:32:33 -0700

    Made the variable modalMask a global varibale on the index.js. Also noticed a new issue where the modal was closing even if you clicked inside the modal. I found the fix for this and implemented it using event.stopimmediatepropagation()

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,23 @@
-document.querySelector('#Book').addEventListener('click', function() {
-   let modalMask = document.querySelector('#modal-mask')
+
+
+document.querySelector('#Book').addEventListener('click', function(e) {
+  
+  let modalMask = document.querySelector('#modal-mask')
    modalMask.style.display= 'flex'
   })
 
-  document.querySelector('#modal-close').addEventListener('click', function() {
+  document.querySelector('#modal-close').addEventListener('click', function(e) {
+    
     let modalMask = document.querySelector('#modal-mask')
     modalMask.style.display= 'none'
    })
 
    document.querySelector('#modal-mask').addEventListener('click', function() {
+     
     let modalMask = document.querySelector('#modal-mask')
     modalMask.style.display= 'none'
    })
 
+document.querySelector('#modal').addEventListener('click', function(e){
+  e.stopImmediatePropagation()
+})

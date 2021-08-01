259f0c8b29f50cb5252a8eb54338b7119226dba7
Mohammed Khan <khanmohammed1@fb.com>
2021-08-01 15:43:13 -0700

    Added a place to enter name, date, phone, etc, and storing it

--- docs/index.js
+++ docs/index.js
@@ -1,23 +1,39 @@
-
+let modalMask = document.querySelector('#modal-mask')
 
 document.querySelector('#Book').addEventListener('click', function(e) {
   
-  let modalMask = document.querySelector('#modal-mask')
+  
    modalMask.style.display= 'flex'
   })
 
-  document.querySelector('#modal-close').addEventListener('click', function(e) {
+document.querySelector('#modal-close').addEventListener('click', function(e) {
     
-    let modalMask = document.querySelector('#modal-mask')
+    
     modalMask.style.display= 'none'
    })
 
-   document.querySelector('#modal-mask').addEventListener('click', function() {
+document.querySelector('#modal-mask').addEventListener('click', function() {
      
-    let modalMask = document.querySelector('#modal-mask')
+    
     modalMask.style.display= 'none'
    })
 
 document.querySelector('#modal').addEventListener('click', function(e){
   e.stopImmediatePropagation()
+})
+
+document.querySelector('#submit').addEventListener('click', function(e) {
+  const nameValue = document.querySelector('#name-input').value
+  const emailValue = document.querySelector('#email-input').value
+  const phoneValue = document.querySelector('#phone-input').value
+  const dateValue = document.querySelector('#date-input').value
+
+  const user = {
+    name: nameValue,
+    email: emailValue,
+    phone: phoneValue,
+    date: dateValue
+  }
+
+  console.log(user)
 })
